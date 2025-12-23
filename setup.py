5c4416fee739663df6e23eb497c13d0c20b2b0a0
Bykamri <mafrizalsuhartono@gmail.com>
2025-12-23 15:24:46 +0700

    first commit

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'medical_delivery_robot'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+        (os.path.join('share', package_name, 'meshes'), glob('meshes/*')),
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='bykamri',
+    maintainer_email='youremail@domain.[Ccom',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
